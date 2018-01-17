d1b945fcddeb2bd7e6ab08e3a4c8527f833917b5
arranlomas <lomasarran1993@googlemail.com>
2018-01-17 19:50:13 +1000

    update user agent

--- src/main/kotlin/server/JsoupWrapper.kt
+++ src/main/kotlin/server/JsoupWrapper.kt
@@ -7,7 +7,7 @@ object JsoupWrapper {
     fun parse(url: String): List<TPBTorrent>{
         val doc = Jsoup.connect(url)
                 .timeout(10 * 1000)
-                .userAgent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36")
+                .userAgent("Mozilla")
                 .get()
         return ScrapeHTML.scrape(doc)
     }
